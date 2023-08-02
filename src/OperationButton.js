82592631103dad82d27bb14afc4960a30211a666
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-08-02 18:24:41 +0200

    React Calculator #2 - finished calculator

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,5 +1,6 @@
 import { ACTIONS } from "./App"
 
 export default function OperationButton({ dispatch, operation }) {
-    return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>{operation}</button>
+    return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>
+        {operation}</button>
 }
